b90a1e070c798ff0a3322cf82b296cd6bbc35557
Marc Cataford <c.marcandre@gmail.com>
2020-12-12 15:15:50 -0500

    chore: set up typescript deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
     extends: [
         "@tophat/eslint-config/base",
         "@tophat/eslint-config/jest",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended"
     ]
 }
