25c5e3c39374d8668ff840cb0470f1629ed1c85b
Marc Cataford <c.marcandre@gmail.com>
2020-12-12 19:02:07 -0500

    chore: linting config

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,5 +6,8 @@ module.exports = {
         "@tophat/eslint-config/jest",
         "plugin:@typescript-eslint/eslint-recommended",
         "plugin:@typescript-eslint/recommended"
-    ]
+    ],
+    settings: {
+        'import/resolver': { node: { extensions: ['.ts'] }}
+    }
 }
