d650690081ff6d43438a6c72714d394f76957e6f
Marc Cataford <mcat@riseup.net>
2023-03-26 11:42:59 -0400

    chore: rebase conflicts

--- src/index.ts
+++ src/index.ts
@@ -3,105 +3,85 @@ import { join, resolve } from 'path'
 
 import { assertInPackageRoot } from './invariants'
 import logger from './logger'
-import {
-    createOrUpdateManifest,
-    getCurrentPackageStats,
-    getPreviousPackageStats,
-    mergeDefaultArguments,
-} from './utils'
+import { createOrUpdateManifest, getCurrentPackageStats, getPreviousPackageStats, mergeDefaultArguments } from './utils'
 
 import type { PackwatchArguments } from './types'
 
 const MANIFEST_FILENAME = '.packwatch.json'
 
-export default async function packwatch(
-    args: Partial<PackwatchArguments>,
-): Promise<void> {
-    const { cwd, isUpdatingManifest } = mergeDefaultArguments(args)
+export default async function packwatch(args: Partial<PackwatchArguments>): Promise<void> {
+	const { cwd, isUpdatingManifest } = mergeDefaultArguments(args)
 
-    const manifestPath = resolve(join(cwd, MANIFEST_FILENAME))
+	const manifestPath = resolve(join(cwd, MANIFEST_FILENAME))
 
-    assertInPackageRoot(cwd)
+	assertInPackageRoot(cwd)
 
-    const currentStats = getCurrentPackageStats(cwd)
+	const currentStats = getCurrentPackageStats(cwd)
 
-    /*
-     * If there is no manifest file yet, we can use the current package stats as
-     * a base to build one. The current package size becomes the limit.
-     */
+	/*
+	 * If there is no manifest file yet, we can use the current package stats as
+	 * a base to build one. The current package size becomes the limit.
+	 */
 
-    if (!existsSync(manifestPath)) {
-        createOrUpdateManifest({ manifestPath, current: currentStats })
-        logger.warn(
-            `📝 No Manifest to compare against! Current package stats written to ${MANIFEST_FILENAME}!\nPackage size (${currentStats.packageSize}) adopted as new limit.`,
-        )
+	if (!existsSync(manifestPath)) {
+		createOrUpdateManifest({ manifestPath, current: currentStats })
+		logger.warn(
+			`📝 No Manifest to compare against! Current package stats written to ${MANIFEST_FILENAME}!\nPackage size (${currentStats.packageSize}) adopted as new limit.`,
+		)
 
-        if (!isUpdatingManifest) {
-            logger.error(
-                '❗ It looks like you ran PackWatch without a manifest. To prevent accidental passes in CI or hooks, packwatch will terminate with an error. If you are running packwatch for the first time in your project, this is expected!',
-            )
-            throw new Error('NO_MANIFEST_NO_UPDATE')
-        }
-        return
-    }
+		if (!isUpdatingManifest) {
+			logger.error(
+				'❗ It looks like you ran PackWatch without a manifest. To prevent accidental passes in CI or hooks, packwatch will terminate with an error. If you are running packwatch for the first time in your project, this is expected!',
+			)
+			throw new Error('NO_MANIFEST_NO_UPDATE')
+		}
+		return
+	}
 
-    const previousStats = getPreviousPackageStats(cwd)
-    const { packageSizeBytes, packageSize } = currentStats
-    const {
-        packageSize: previousSize,
-        packageSizeBytes: previousSizeBytes,
-        limit,
-        limitBytes,
-    } = previousStats
-    const hasExceededLimit = limitBytes && packageSizeBytes > limitBytes
+	const previousStats = getPreviousPackageStats(cwd)
+	const { packageSizeBytes, packageSize } = currentStats
+	const { packageSize: previousSize, packageSizeBytes: previousSizeBytes, limit, limitBytes } = previousStats
+	const hasExceededLimit = limitBytes && packageSizeBytes > limitBytes
 
-    /*
-     * If we are updating the manifest, we can write right away and terminate.
-     */
+	/*
+	 * If we are updating the manifest, we can write right away and terminate.
+	 */
 
-    if (isUpdatingManifest) {
-        createOrUpdateManifest({
-            previous: previousStats,
-            current: currentStats,
-            updateLimit: true,
-            manifestPath,
-        })
-        logger.log(
-            `📝 Updated the manifest! Package size: ${packageSize}, Limit: ${packageSize}`,
-        )
-        return
-    }
+	if (isUpdatingManifest) {
+		createOrUpdateManifest({
+			previous: previousStats,
+			current: currentStats,
+			updateLimit: true,
+			manifestPath,
+		})
+		logger.log(`📝 Updated the manifest! Package size: ${packageSize}, Limit: ${packageSize}`)
+		return
+	}
 
-    /*
-     * If there is a manifest file and the current package busts its limit
-     * we signal it and terminate with an error.
-     */
+	/*
+	 * If there is a manifest file and the current package busts its limit
+	 * we signal it and terminate with an error.
+	 */
 
-    if (hasExceededLimit) {
-        logger.error(
-            `🔥🔥📦🔥🔥 Your package exceeds the limit set in ${MANIFEST_FILENAME}! ${packageSize} > ${limit}\nEither update the limit by using the --update-manifest flag or trim down your packed files!`,
-        )
-        throw new Error('PACKAGE_EXCEEDS_LIMIT')
-    }
+	if (hasExceededLimit) {
+		logger.error(
+			`🔥🔥📦🔥🔥 Your package exceeds the limit set in ${MANIFEST_FILENAME}! ${packageSize} > ${limit}\nEither update the limit by using the --update-manifest flag or trim down your packed files!`,
+		)
+		throw new Error('PACKAGE_EXCEEDS_LIMIT')
+	}
 
-    /*
-     * If there is a manifest file and the limit is not busted, we give
-     * the user some feedback on how the current package compares with
-     * the previous one.
-     */
+	/*
+	 * If there is a manifest file and the limit is not busted, we give
+	 * the user some feedback on how the current package compares with
+	 * the previous one.
+	 */
 
-    if (packageSizeBytes > previousSizeBytes) {
-        logger.log(
-            `📦 👀 Your package grew! ${packageSize} > ${previousSize} (Limit: ${limit})`,
-        )
-    } else if (packageSizeBytes < previousSizeBytes) {
-        logger.log(
-            `📦 💯 Your package shrank! ${packageSize} < ${previousSize} (Limit: ${limit})`,
-        )
-    } else {
-        logger.log(
-            `📦 Nothing to report! Your package is the same size as the latest manifest reports! (Limit: ${limit})`,
-        )
-    }
-    return
+	if (packageSizeBytes > previousSizeBytes) {
+		logger.log(`📦 👀 Your package grew! ${packageSize} > ${previousSize} (Limit: ${limit})`)
+	} else if (packageSizeBytes < previousSizeBytes) {
+		logger.log(`📦 💯 Your package shrank! ${packageSize} < ${previousSize} (Limit: ${limit})`)
+	} else {
+		logger.log(`📦 Nothing to report! Your package is the same size as the latest manifest reports! (Limit: ${limit})`)
+	}
+	return
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -9,255 +9,226 @@ import type { Report } from '../src/types'
 let workspace: string | null
 
 function getActualPackageSizeByNodeVersion(nodeVersion: string): string {
-    if (nodeVersion.startsWith('v14')) return '160'
-    else if (nodeVersion.startsWith('v16')) return '157'
-    else if (nodeVersion.startsWith('v18')) return '157'
+	if (nodeVersion.startsWith('v14')) return '160'
+	else if (nodeVersion.startsWith('v16')) return '157'
+	else if (nodeVersion.startsWith('v18')) return '157'
 
-    return 'unknown'
+	return 'unknown'
 }
 
 async function prepareWorkspace(): Promise<string> {
-    const workspacePath = await fs.mkdtemp(`${tmpdir()}/`)
-    workspace = workspacePath
-    return workspacePath
+	const workspacePath = await fs.mkdtemp(`${tmpdir()}/`)
+	workspace = workspacePath
+	return workspacePath
 }
 
 async function cleanUpWorkspace(paths: string[]): Promise<void> {
-    await Promise.all(
-        paths.map(async (path) => fs.rmdir(path, { recursive: true })),
-    )
+	await Promise.all(paths.map(async (path) => fs.rmdir(path, { recursive: true })))
 }
 
 async function createFile(path: string, content: string): Promise<void> {
-    await fs.writeFile(path, content)
+	await fs.writeFile(path, content)
 }
 
 async function createPackageJson(cwd: string): Promise<void> {
-    const path = resolve(join(cwd, 'package.json'))
-    await createFile(
-        path,
-        '{ "name": "wow", "version": "0.0.0", "files": ["!.packwatch.json"] }',
-    )
+	const path = resolve(join(cwd, 'package.json'))
+	await createFile(path, '{ "name": "wow", "version": "0.0.0", "files": ["!.packwatch.json"] }')
 }
 
-async function createManifest(
-    cwd: string,
-    configuration: Report,
-): Promise<void> {
-    const path = resolve(join(cwd, '.packwatch.json'))
-    await createFile(path, JSON.stringify(configuration))
+async function createManifest(cwd: string, configuration: Report): Promise<void> {
+	const path = resolve(join(cwd, '.packwatch.json'))
+	await createFile(path, JSON.stringify(configuration))
 }
 
 describe('Packwatch', () => {
-    const actualSize = getActualPackageSizeByNodeVersion(process.version)
-    afterEach(async () => {
-        jest.restoreAllMocks()
+	const actualSize = getActualPackageSizeByNodeVersion(process.version)
+	afterEach(async () => {
+		jest.restoreAllMocks()
 
-        if (workspace) {
-            await cleanUpWorkspace([workspace])
-            workspace = null
-        }
-    })
+		if (workspace) {
+			await cleanUpWorkspace([workspace])
+			workspace = null
+		}
+	})
 
-    it('warns the user and errors if run away from package.json', async () => {
-        const workspacePath = await prepareWorkspace()
-        const mockLogger = jest.spyOn(console, 'log')
+	it('warns the user and errors if run away from package.json', async () => {
+		const workspacePath = await prepareWorkspace()
+		const mockLogger = jest.spyOn(console, 'log')
 
-        await expect(async () =>
-            packwatch({ cwd: workspacePath }),
-        ).rejects.toThrow('NOT_IN_PACKAGE_ROOT')
+		await expect(async () => packwatch({ cwd: workspacePath })).rejects.toThrow('NOT_IN_PACKAGE_ROOT')
 
-        expect(mockLogger.mock.calls).toHaveLength(1)
-        expect(mockLogger.mock.calls[0][0]).toEqual(
-            expect.stringMatching(
-                'There is no package.json file here. Are you in the root directory of your project?',
-            ),
-        )
-    })
+		expect(mockLogger.mock.calls).toHaveLength(1)
+		expect(mockLogger.mock.calls[0][0]).toEqual(
+			expect.stringMatching('There is no package.json file here. Are you in the root directory of your project?'),
+		)
+	})
 
-    describe('without manifest', () => {
-        it('generates the initial manifest properly', async () => {
-            const workspacePath = await prepareWorkspace()
-            await createPackageJson(workspacePath)
+	describe('without manifest', () => {
+		it('generates the initial manifest properly', async () => {
+			const workspacePath = await prepareWorkspace()
+			await createPackageJson(workspacePath)
 
-            await expect(async () =>
-                packwatch({ cwd: workspacePath }),
-            ).rejects.toThrow('NO_MANIFEST_NO_UPDATE')
+			await expect(async () => packwatch({ cwd: workspacePath })).rejects.toThrow('NO_MANIFEST_NO_UPDATE')
 
-            const generatedManifest = await fs.readFile(
-                resolve(join(workspacePath, '.packwatch.json')),
-                { encoding: 'utf8' },
-            )
+			const generatedManifest = await fs.readFile(resolve(join(workspacePath, '.packwatch.json')), { encoding: 'utf8' })
 
-            expect(generatedManifest).toBe(
-                `{"limit":"${actualSize} B","packageSize":"${actualSize} B","unpackedSize":"68 B"}`,
-            )
-        })
+			expect(generatedManifest).toBe(
+				`{"limit":"${actualSize} B","packageSize":"${actualSize} B","unpackedSize":"68 B"}`,
+			)
+		})
 
-        it('outputs expected messaging', async () => {
-            const workspacePath = await prepareWorkspace()
-            const mockWarn = jest.spyOn(console, 'warn')
-            const mockError = jest.spyOn(console, 'error')
-            await createPackageJson(workspacePath)
+		it('outputs expected messaging', async () => {
+			const workspacePath = await prepareWorkspace()
+			const mockWarn = jest.spyOn(console, 'warn')
+			const mockError = jest.spyOn(console, 'error')
+			await createPackageJson(workspacePath)
 
-            await expect(async () =>
-                packwatch({ cwd: workspacePath }),
-            ).rejects.toThrow()
+			await expect(async () => packwatch({ cwd: workspacePath })).rejects.toThrow()
 
-            expect(mockWarn.mock.calls).toHaveLength(1)
-            expect(mockWarn.mock.calls[0][0]).toEqual(
-                expect.stringMatching(
-                    /No Manifest to compare against! Current package stats written to \.packwatch\.json!\nPackage size \(\d+ B\) adopted as new limit\./,
-                ),
-            )
-            expect(mockError.mock.calls).toHaveLength(1)
-            expect(mockError.mock.calls[0][0]).toEqual(
-                expect.stringMatching(
-                    'It looks like you ran PackWatch without a manifest. To prevent accidental passes in CI or hooks, packwatch will terminate with an error. If you are running packwatch for the first time in your project, this is expected!',
-                ),
-            )
-        })
+			expect(mockWarn.mock.calls).toHaveLength(1)
+			expect(mockWarn.mock.calls[0][0]).toEqual(
+				expect.stringMatching(
+					/No Manifest to compare against! Current package stats written to \.packwatch\.json!\nPackage size \(\d+ B\) adopted as new limit\./,
+				),
+			)
+			expect(mockError.mock.calls).toHaveLength(1)
+			expect(mockError.mock.calls[0][0]).toEqual(
+				expect.stringMatching(
+					'It looks like you ran PackWatch without a manifest. To prevent accidental passes in CI or hooks, packwatch will terminate with an error. If you are running packwatch for the first time in your project, this is expected!',
+				),
+			)
+		})
 
-        it('outputs expected messaging when not updating the manifest', async () => {
-            const mockWarn = jest.spyOn(console, 'warn')
-            const workspacePath = await prepareWorkspace()
+		it('outputs expected messaging when not updating the manifest', async () => {
+			const mockWarn = jest.spyOn(console, 'warn')
+			const workspacePath = await prepareWorkspace()
 
-            await createPackageJson(workspacePath)
+			await createPackageJson(workspacePath)
 
-            await packwatch({ cwd: workspacePath, isUpdatingManifest: true })
+			await packwatch({ cwd: workspacePath, isUpdatingManifest: true })
 
-            expect(mockWarn.mock.calls).toHaveLength(1)
-            expect(mockWarn.mock.calls[0][0]).toEqual(
-                expect.stringMatching(
-                    /No Manifest to compare against! Current package stats written to \.packwatch\.json!\nPackage size \(\d+ B\) adopted as new limit\./,
-                ),
-            )
-        })
-    })
+			expect(mockWarn.mock.calls).toHaveLength(1)
+			expect(mockWarn.mock.calls[0][0]).toEqual(
+				expect.stringMatching(
+					/No Manifest to compare against! Current package stats written to \.packwatch\.json!\nPackage size \(\d+ B\) adopted as new limit\./,
+				),
+			)
+		})
+	})
 
-    describe('with manifest', () => {
-        it('messages when the size is equal to the limit', async () => {
-            const workspacePath = await prepareWorkspace()
-            const mockLogger = jest.spyOn(console, 'log')
-            await createPackageJson(workspacePath)
-            await createManifest(workspacePath, {
-                limit: `${actualSize}B`,
-                packageSize: `${actualSize}B`,
-                packageSizeBytes: Number(actualSize),
-                unpackedSize: '150B',
-                unpackedSizeBytes: 150,
-            })
-            await packwatch({ cwd: workspacePath })
-            expect(mockLogger.mock.calls).toHaveLength(1)
-            expect(mockLogger.mock.calls[0][0]).toEqual(
-                expect.stringMatching(
-                    /Nothing to report! Your package is the same size as the latest manifest reports!/,
-                ),
-            )
-        })
+	describe('with manifest', () => {
+		it('messages when the size is equal to the limit', async () => {
+			const workspacePath = await prepareWorkspace()
+			const mockLogger = jest.spyOn(console, 'log')
+			await createPackageJson(workspacePath)
+			await createManifest(workspacePath, {
+				limit: `${actualSize}B`,
+				packageSize: `${actualSize}B`,
+				packageSizeBytes: Number(actualSize),
+				unpackedSize: '150B',
+				unpackedSizeBytes: 150,
+			})
+			await packwatch({ cwd: workspacePath })
+			expect(mockLogger.mock.calls).toHaveLength(1)
+			expect(mockLogger.mock.calls[0][0]).toEqual(
+				expect.stringMatching(/Nothing to report! Your package is the same size as the latest manifest reports!/),
+			)
+		})
 
-        it('messages when the size is lower than the limit (no growth)', async () => {
-            const workspacePath = await prepareWorkspace()
-            const mockLogger = jest.spyOn(console, 'log')
-            await createPackageJson(workspacePath)
-            await createManifest(workspacePath, {
-                limit: '170B',
-                packageSize: `${actualSize}B`,
-                packageSizeBytes: Number(actualSize),
-                unpackedSize: '150B',
-                unpackedSizeBytes: 150,
-            })
+		it('messages when the size is lower than the limit (no growth)', async () => {
+			const workspacePath = await prepareWorkspace()
+			const mockLogger = jest.spyOn(console, 'log')
+			await createPackageJson(workspacePath)
+			await createManifest(workspacePath, {
+				limit: '170B',
+				packageSize: `${actualSize}B`,
+				packageSizeBytes: Number(actualSize),
+				unpackedSize: '150B',
+				unpackedSizeBytes: 150,
+			})
 
-            await packwatch({ cwd: workspacePath })
-            expect(mockLogger.mock.calls).toHaveLength(1)
-            expect(mockLogger.mock.calls[0][0]).toEqual(
-                expect.stringMatching(
-                    /Nothing to report! Your package is the same size as the latest manifest reports! \(Limit: 170B\)/,
-                ),
-            )
-        })
-        it('messages when the size is lower than the limit (growth)', async () => {
-            const workspacePath = await prepareWorkspace()
-            const mockLogger = jest.spyOn(console, 'log')
-            await createPackageJson(workspacePath)
-            await createManifest(workspacePath, {
-                limit: '180B',
-                packageSize: '150B',
-                packageSizeBytes: 150,
-                unpackedSize: '140B',
-                unpackedSizeBytes: 140,
-            })
+			await packwatch({ cwd: workspacePath })
+			expect(mockLogger.mock.calls).toHaveLength(1)
+			expect(mockLogger.mock.calls[0][0]).toEqual(
+				expect.stringMatching(
+					/Nothing to report! Your package is the same size as the latest manifest reports! \(Limit: 170B\)/,
+				),
+			)
+		})
+		it('messages when the size is lower than the limit (growth)', async () => {
+			const workspacePath = await prepareWorkspace()
+			const mockLogger = jest.spyOn(console, 'log')
+			await createPackageJson(workspacePath)
+			await createManifest(workspacePath, {
+				limit: '180B',
+				packageSize: '150B',
+				packageSizeBytes: 150,
+				unpackedSize: '140B',
+				unpackedSizeBytes: 140,
+			})
 
-            await packwatch({ cwd: workspacePath })
-            expect(mockLogger.mock.calls).toHaveLength(1)
-            expect(mockLogger.mock.calls[0][0]).toEqual(
-                expect.stringMatching(
-                    /Your package grew! \d+ B > 150B \(Limit: 180B\)/,
-                ),
-            )
-        })
-        it('messages when the size is lower than the limit (shrinkage)', async () => {
-            const workspacePath = await prepareWorkspace()
-            const mockLogger = jest.spyOn(console, 'log')
-            await createPackageJson(workspacePath)
-            await createManifest(workspacePath, {
-                limit: '180B',
-                packageSize: '170B',
-                packageSizeBytes: 170,
-                unpackedSize: '140B',
-                unpackedSizeBytes: 140,
-            })
+			await packwatch({ cwd: workspacePath })
+			expect(mockLogger.mock.calls).toHaveLength(1)
+			expect(mockLogger.mock.calls[0][0]).toEqual(
+				expect.stringMatching(/Your package grew! \d+ B > 150B \(Limit: 180B\)/),
+			)
+		})
+		it('messages when the size is lower than the limit (shrinkage)', async () => {
+			const workspacePath = await prepareWorkspace()
+			const mockLogger = jest.spyOn(console, 'log')
+			await createPackageJson(workspacePath)
+			await createManifest(workspacePath, {
+				limit: '180B',
+				packageSize: '170B',
+				packageSizeBytes: 170,
+				unpackedSize: '140B',
+				unpackedSizeBytes: 140,
+			})
 
-            await packwatch({ cwd: workspacePath })
-            expect(mockLogger.mock.calls).toHaveLength(1)
-            expect(mockLogger.mock.calls[0][0]).toEqual(
-                expect.stringMatching(
-                    /Your package shrank! \d+ B < 170B \(Limit: 180B\)/,
-                ),
-            )
-        })
-        it('messages when the size exceeds the limit', async () => {
-            const workspacePath = await prepareWorkspace()
-            const mockError = jest.spyOn(console, 'error')
-            await createPackageJson(workspacePath)
-            await createManifest(workspacePath, {
-                limit: '10B',
-                packageSize: '170B',
-                packageSizeBytes: 170,
-                unpackedSize: '140B',
-                unpackedSizeBytes: 140,
-            })
+			await packwatch({ cwd: workspacePath })
+			expect(mockLogger.mock.calls).toHaveLength(1)
+			expect(mockLogger.mock.calls[0][0]).toEqual(
+				expect.stringMatching(/Your package shrank! \d+ B < 170B \(Limit: 180B\)/),
+			)
+		})
+		it('messages when the size exceeds the limit', async () => {
+			const workspacePath = await prepareWorkspace()
+			const mockError = jest.spyOn(console, 'error')
+			await createPackageJson(workspacePath)
+			await createManifest(workspacePath, {
+				limit: '10B',
+				packageSize: '170B',
+				packageSizeBytes: 170,
+				unpackedSize: '140B',
+				unpackedSizeBytes: 140,
+			})
 
-            await expect(async () =>
-                packwatch({ cwd: workspacePath }),
-            ).rejects.toThrow('PACKAGE_EXCEEDS_LIMIT')
-            expect(mockError.mock.calls).toHaveLength(1)
-            expect(mockError.mock.calls[0][0]).toEqual(
-                expect.stringMatching(
-                    /Your package exceeds the limit set in \.packwatch\.json! \d+ B > 10B\nEither update the limit by using the --update-manifest flag or trim down your packed files!/,
-                ),
-            )
-        })
+			await expect(async () => packwatch({ cwd: workspacePath })).rejects.toThrow('PACKAGE_EXCEEDS_LIMIT')
+			expect(mockError.mock.calls).toHaveLength(1)
+			expect(mockError.mock.calls[0][0]).toEqual(
+				expect.stringMatching(
+					/Your package exceeds the limit set in \.packwatch\.json! \d+ B > 10B\nEither update the limit by using the --update-manifest flag or trim down your packed files!/,
+				),
+			)
+		})
 
-        it('messages when updating the manifest', async () => {
-            const workspacePath = await prepareWorkspace()
-            const mockLogger = jest.spyOn(console, 'log')
-            await createPackageJson(workspacePath)
-            await createManifest(workspacePath, {
-                limit: '10B',
-                packageSize: '170B',
-                packageSizeBytes: 170,
-                unpackedSize: '140B',
-                unpackedSizeBytes: 140,
-            })
+		it('messages when updating the manifest', async () => {
+			const workspacePath = await prepareWorkspace()
+			const mockLogger = jest.spyOn(console, 'log')
+			await createPackageJson(workspacePath)
+			await createManifest(workspacePath, {
+				limit: '10B',
+				packageSize: '170B',
+				packageSizeBytes: 170,
+				unpackedSize: '140B',
+				unpackedSizeBytes: 140,
+			})
 
-            await packwatch({ cwd: workspacePath, isUpdatingManifest: true })
-            expect(mockLogger.mock.calls).toHaveLength(1)
-            expect(mockLogger.mock.calls[0][0]).toEqual(
-                expect.stringMatching(
-                    /Updated the manifest! Package size: \d+ B, Limit: \d+ B/,
-                ),
-            )
-        })
-    })
+			await packwatch({ cwd: workspacePath, isUpdatingManifest: true })
+			expect(mockLogger.mock.calls).toHaveLength(1)
+			expect(mockLogger.mock.calls[0][0]).toEqual(
+				expect.stringMatching(/Updated the manifest! Package size: \d+ B, Limit: \d+ B/),
+			)
+		})
+	})
 })
